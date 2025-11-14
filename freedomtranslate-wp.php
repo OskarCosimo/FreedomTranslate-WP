40cbb61ede6479b3a056603cf3cdd0c3fb5677fc
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2025-11-14 19:55:09 +0100

    Bump version to 1.4.5

--- freedomtranslate-wp.php
+++ freedomtranslate-wp.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: FreedomTranslate WP
 Description: Translate on-the-fly with LibreTranslate (localhost:5000) or remote URL with API + cache and language selection
-Version: 1.4.4
+Version: 1.4.5
 Author: thefreedom
 License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
